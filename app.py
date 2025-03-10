f20be38decaf80388e49b8a491d4c2587dbe6049
Abdelmonaam Kallali <abdelmonaam.kallali@ssc-spc.gc.ca>
2025-03-10 13:09:52 -0400

    Added local image paths

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ class URLStatus:
                     "Connection": "keep-alive"
                 }
                 status_code = response.status_code
-                status = """<img src="images/green_light.jpg" alt="Green Light" width="20px"/>"""
+                status = """<img src="/images/green_light.jpg" alt="Green Light" width="20px"/>"""
                 status_color = "white"
                 
                 if (status_code == 200):
@@ -90,26 +90,26 @@ class URLStatus:
                     self.down_urls.pop(url, None)
                     self.emails_sent.pop(url, None)
                 else:
-                    status = """<img src="images/red_light.jpg" alt="Red Light" width="20px"/>"""
+                    status = """<img src="/images/red_light.jpg" alt="Red Light" width="20px"/>"""
                     if url not in self.down_urls:
                         self.down_urls[url] = datetime.now(est_timezone)
                 
             except requests.exceptions.ConnectionError as e:
-                status = """<img src="images/red_light.jpg" alt="Red Light" width="20px"/>"""
+                status = """<img src="/images/red_light.jpg" alt="Red Light" width="20px"/>"""
                 status_color = "white"
                 status_code = "N/A"
 
                 if url not in self.down_urls:
                     self.down_urls[url] = datetime.now(est_timezone)
             except requests.exceptions.Timeout as e:
-                status = """<img src="images/red_light.jpg" alt="Red Light" width="20px"/>"""
+                status = """<img src="/images/red_light.jpg" alt="Red Light" width="20px"/>"""
                 status_color = "white"
                 status_code = e.response.status_code if e.response else "Timeout"
                 
                 if url not in self.down_urls:
                     self.down_urls[url] = datetime.now(est_timezone)
             except requests.exceptions.HTTPError as e:
-                status = """<img src="images/red_light.jpg" alt="Red Light" width="20px"/>"""
+                status = """<img src="/images/red_light.jpg" alt="Red Light" width="20px"/>"""
                 status_color = "white"
                 status_code = e.response.status_code if e.response else "Timeout"
                 
@@ -164,7 +164,7 @@ def init_html():
     <meta http-equiv="refresh" content="{refresh_time}" />
 </head>
 <body>
-    <h1>Testing CIO Cloud Apps</h1>
+    <h1><img src="/images/SSC_Logo.png" alt="Red Light" width="20px"/>Testing CIO Cloud Apps</h1>
     """
 
 
