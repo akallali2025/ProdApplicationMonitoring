afca8ef998e144ea1f830b941aa2d4d33363e9e9
slow6565 <simonlowe65@gmail.com>
2025-04-03 12:38:32 -0400

    checks for db

--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ import os
 
 def init_db(db_path="my_datebase.db", script_path="create_tables.sql"):
 
-    #check if db exists 
+    #check if db exists -> test
     if os.path.exists(db_path):
         return 
 
